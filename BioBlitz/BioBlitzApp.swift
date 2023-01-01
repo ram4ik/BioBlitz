dd10875a9274c3f88f68d15a751abee1ac2be499
Ramill <ramill_ibragimov@icloud.com>
2023-01-01 21:18:00 +0200

    Initial Commit

---
+++ BioBlitz/BioBlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioBlitzApp.swift
+//  BioBlitz
+//
+//  Created by Ramill Ibragimov on 01.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct BioBlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
